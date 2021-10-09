5eeb3dfedb2cc4df967c59ea6165a662c6ca8532
kevin Higuera <kevin.higuera.mt@correo.usa.edu.co>
2021-10-08 19:14:14 -0500

    creacion funcion restar

--- calculadora_para_bobos.py
+++ calculadora_para_bobos.py
@@ -1,8 +1,13 @@
 def sumar(n1,n2):     
     return n1+n2  
+
+def restar(n1,n2):
+    return n1-n2
+
 print("calculadora ejemplo") 
 print() 
 print("1). SUMAR") 
+print("2). RESTAR")
 
 op=int(input("digite una opcion: ")) 
 n1=int(input("digite el numero: ")) 
@@ -13,3 +18,9 @@ if op==1:
          
 print("el resultado es: ", res)
 print("este es un cambio que hizo om5")
+
+if op==2:
+    res=restar(n1,n2)
+
+print("el resultado es: ", res)
+print("este es un cambio que hizo kevin :v")
